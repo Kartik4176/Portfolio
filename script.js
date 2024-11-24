6e794c438dfe1c92ed1b93e595f721d4d5ff196d
Kartik <k.kartik4176@gmail.com>
2024-11-24 22:59:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby5OURRvfHFXby_xDdC4_o865S44OyMgoG4I4QIz__z14ihoS0VkHhANPnV1l1p3jtg/exec'
+
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        msg.innerHTML = "Message sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
